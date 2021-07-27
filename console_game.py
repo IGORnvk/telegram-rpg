d78cfd6b309d4827fdbaae9a42c61af9d5e975e6
user <igor.kn1911@gmail.com>
2021-07-27 10:06:24 +0300

    changed

--- console_game.py
+++ console_game.py
@@ -1,16 +1,29 @@
 from fighter import Fighter
+from item import PotionEffectType
 from move import UseActiveSkill
 from player import Player
 from skill import ActiveSkillEffectType
+from item import Potion
 
 
 def check_skill_type(skill):
     if skill.effect.type == ActiveSkillEffectType.damage.value:
         return "Урон: {}".format(skill.effect.value)
     if skill.effect.type == ActiveSkillEffectType.heal.value:
-        return "Восстанавливает: {}".format(skill.effect.value)
+        return "Восстановление: {}".format(skill.effect.value)
     if skill.effect.type == ActiveSkillEffectType.debuff_defense.value:
         return "Повреждение защиты: {}".format(skill.effect.value)
+
+
+def check_potion_type(potion):
+    if potion.effect.effect_type == PotionEffectType.power.value:
+        return "Увеличение Силы: {}".format(potion.effect.value)
+    if potion.effect.effect_type == PotionEffectType.defense.value:
+        return "Увеличение Защиты: {}".format(potion.effect.value)
+    if potion.effect.effect_type == PotionEffectType.health.value:
+        return "Увеличение Здоровья: {}".format(potion.effect.value)
+    if potion.effect.effect_type == PotionEffectType.mana.value:
+        return "Увеличение Маны: {}".format(potion.effect.value)
 
 
 def print_player_info(player: Player):
@@ -29,7 +42,11 @@ def print_player_info(player: Player):
         print("\tНомер: {}\n\t\tНазвание: {}\n\t\tРанг: {}\n\t\tОписание: {}\n\t\t{}".format(i, skill.name, skill.rang,
                                                                                              skill.description,
                                                                                              check_skill_type(skill)))
-
+    potions = [i for i in player.inventory.items if type(i) is Potion]
+    print("Зелья:")
+    for i, potion in enumerate(potions):
+        print("\tНомер: {}\n\t\tНазвание: {}\n\t\tРанг: {}\n\t\t{}".format(i, potion.name, potion.rang,
+                                                                           check_potion_type(potion)))
     # print(player.username, player.level, player.cur_health, player.cur_mana, player.equipment, player.active_skills,
     #      player.passive_skills)
 
@@ -47,7 +64,23 @@ class ConsoleGame:
             defender = self.fighter.get_defender()
             print_player_info(to_move)
             print_player_info(defender)
-            number = int(input())
-            print("Выбранный скил: {}".format(to_move.active_skills[number].name))
-            is_finished, damage = self.fighter.step(UseActiveSkill(to_move.active_skills[number]))
-            print("Нанесенный урон: {}".format(damage))
+            while True:
+                move_type = input()
+                if move_type == "potion":
+                    try:
+                        number = int(input())
+                        potions = [i for i in to_move.inventory.items if type(i) is Potion]
+                        print("Выбранное зелье: {}".format(potions[number]))
+                        break
+                    except:
+                        print("Введите номер зелья.")
+                if move_type == "skill":
+                    try:
+                        number = int(input())
+                        print("Выбранный скил: {}".format(to_move.active_skills[number].name))
+                        is_finished, damage = self.fighter.step(UseActiveSkill(to_move.active_skills[number]))
+                        print("Нанесенный урон: {}".format(damage))
+                        break
+                    except:
+                        print("Введите номер скила.")
+                print("Введите 'potion' или 'skill' в зависимости от того, что вы хотите использовать.")
--- console_test.py
+++ console_test.py
@@ -12,8 +12,8 @@ player1 = Player(10, 'Ivan', 63, 63, 20, 15, 40, 40, 10, [
 
         ActiveSkill('knife', 'F', 'first skill', 5, ActiveSkillEffect(15, ActiveSkillEffectType.damage.value))
     ],
-                 [PassiveSkill('power increase', 'F', 'first passive skill', PassiveSkillEffect(5, PassiveSkillEffectType.increase_power.value))], equipment1, Inventory([]), [Potion('power buff', 'F', PotionEffect(500, PotionEffectType.power.value))])
+                 [PassiveSkill('power increase', 'F', 'first passive skill', PassiveSkillEffect(5, PassiveSkillEffectType.increase_power.value))], equipment1, Inventory([Potion('power buff', 'F', PotionEffect(500, PotionEffectType.power.value))]), [])
 player2 = Player(12, 'Igor', 60, 60, 21, 16, 45, 45, 11, [ActiveSkill('knife', 'F', 'first skill', 5, ActiveSkillEffect(15, ActiveSkillEffectType.damage.value))],
-                 [PassiveSkill('defense increase', 'F', 'increases your defense', PassiveSkillEffect(6, PassiveSkillEffectType.increase_defense.value))], equipment2, Inventory([]), [Potion('defense buff', 'F', PotionEffect(600, PotionEffectType.defense.value))])
+                 [PassiveSkill('defense increase', 'F', 'increases your defense', PassiveSkillEffect(6, PassiveSkillEffectType.increase_defense.value))], equipment2, Inventory([Potion('defense buff', 'F', PotionEffect(600, PotionEffectType.defense.value))]), [])
 console_game = ConsoleGame(player1, player2)
 console_game.run()
